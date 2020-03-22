d23e4b056e19f40ba498f86ede9dfc28228b7054
vineet23 <vineet23@users.noreply.github.com>
2020-03-23 00:25:42 +0530

    dataupdate

---
+++ Corona Data/dataupdate.py
@@ -0,0 +1,81 @@
+import requests
+import json
+
+url = "https://www.bing.com/covid/data"
+
+response = requests.get(url)
+
+resp = response.json()
+
+with open("results2.json",'w') as f:
+    json.dump(resp,f)
+
+with open("results2.json",'r') as f:
+    ditc = json.load(f)
+
+data = []
+
+total = int(ditc['totalConfirmed'])
+deaths = int(ditc['totalDeaths'])
+recov = int(ditc['totalRecovered'])
+active = total - deaths - recov
+print(str(total)+" "+str(deaths)+" "+str(recov)+" "+str(active)+"\n")
+areas = list(ditc['areas'])
+for area in areas:
+    totald = area['totalConfirmed']
+    death = area['totalDeaths']
+    rec = area['totalRecovered']
+    active = int(totald)-int(death)-int(rec)
+    name = area['displayName']
+    lat = area['lat']
+    lng = area['long']
+    if totald<10:
+        for i in range(totald):
+            data.append({"latitude":lat,"longitude":lng,"place":name,"usertype":"admin","condition":"active"})
+    else:    
+        for i in range((totald%10)+2):
+            data.append({"latitude":lat,"longitude":lng,"place":name,"usertype":"admin","condition":"active"})
+    aras = list(area['areas'])
+    for ar in aras:
+        totald = ar['totalConfirmed']
+        death = ar['totalDeaths']
+        rec = ar['totalRecovered']
+        active = int(totald)-int(death)-int(rec)
+        name = ar['displayName']
+        lat = ar['lat']
+        lng = ar['long']
+        if totald<10:
+            for i in range(totald):
+                data.append({"latitude":lat,"longitude":lng,"place":name,"usertype":"admin","condition":"active"})
+        else:    
+            for i in range((totald%10)+2):
+                data.append({"latitude":lat,"longitude":lng,"place":name,"usertype":"admin","condition":"active"})
+        region = list(ar['areas'])
+        for rg in region:
+            rtotald = rg['totalConfirmed']
+            rdeath = rg['totalDeaths']
+            rrec = rg['totalRecovered']
+            ractive = int(totald)-int(death)-int(rec)
+            rname = rg['displayName']
+            rlat = rg['lat']
+            rlng = rg['long']
+            if rtotald<10:
+                for i in range(rtotald):
+                    data.append({"latitude":rlat,"longitude":rlng,"place":rname,"usertype":"admin","condition":"active"})
+            else:    
+                for i in range((rtotald%10)+2):
+                    data.append({"latitude":rlat,"longitude":rlng,"place":rname,"usertype":"admin","condition":"active"})
+
+url = "https://api.papersqueeze.com/corona/addlocation/"
+headers = {"Authorization":"Token da0646e9548cb3fca6ac7e15ed2810a2fd53d2f5"}
+dat = {"usertype":"admin","condition":"active","place":"","latitude":"","longitude":""}
+
+
+#response = requests.post(url,headers=headers,data=data)
+#print(response.json())
+for row in data:
+    dat['place'] = row["place"]
+    dat['latitude'] = float(row["latitude"])
+    dat['longitude'] = float(row["longitude"])
+    response = requests.post(url,headers=headers,data=data)
+    print(response.json())
--- Corona Data/main.py
+++ Corona Data/main.py
@@ -7,36 +7,45 @@ response = requests.get(url)
 
 resp = response.json()
 
-with open("results.json",'w') as f:
+with open("results2.json",'w') as f:
     json.dump(resp,f)
 
-with open("results.json",'r') as f:
+with open("results2.json",'r') as f:
     ditc = json.load(f)
 
-with open("result.txt",'w') as fh:
-    total = int(ditc['totalConfirmed'])
-    deaths = int(ditc['totalDeaths'])
-    recov = int(ditc['totalRecovered'])
-    active = total - deaths - recov
-    fh.write(str(total)+" "+str(deaths)+" "+str(recov)+" "+str(active)+"\n")
-    areas = list(ditc['areas'])
-    for area in areas:
-        totald = area['totalConfirmed']
-        death = area['totalDeaths']
-        rec = area['totalRecovered']
+total = int(ditc['totalConfirmed'])
+deaths = int(ditc['totalDeaths'])
+recov = int(ditc['totalRecovered'])
+active = total - deaths - recov
+print(str(total)+" "+str(deaths)+" "+str(recov)+" "+str(active)+"\n")
+areas = list(ditc['areas'])
+for area in areas:
+    totald = area['totalConfirmed']
+    death = area['totalDeaths']
+    rec = area['totalRecovered']
+    active = int(totald)-int(death)-int(rec)
+    name = area['displayName']
+    lat = area['lat']
+    lng = area['long']
+    print(name+" "+str(lat)+" "+str(lng)+" "+str(totald)+" "+str(death)+" "+str(rec)+" "+str(active)+" \n")
+    aras = list(area['areas'])
+    for ar in aras:
+        totald = ar['totalConfirmed']
+        death = ar['totalDeaths']
+        rec = ar['totalRecovered']
         active = int(totald)-int(death)-int(rec)
-        name = area['displayName']
-        lat = area['lat']
-        lng = area['long']
-        fh.write(name+" "+str(lat)+" "+str(lng)+" "+str(totald)+" "+str(death)+" "+str(rec)+" "+str(active)+" \n")
-        aras = list(area['areas'])
-        for ar in aras:
-            totald = ar['totalConfirmed']
-            death = ar['totalDeaths']
-            rec = ar['totalRecovered']
-            active = int(totald)-int(death)-int(rec)
-            name = ar['displayName']
-            lat = ar['lat']
-            lng = ar['long']
-            fh.write("\t"+name+" "+str(lat)+" "+str(lng)+" "+str(totald)+" "+str(death)+" "+str(rec)+" "+str(active)+" \n")
+        name = ar['displayName']
+        lat = ar['lat']
+        lng = ar['long']
+        print(name+" "+str(lat)+" "+str(lng)+" "+str(totald)+" "+str(death)+" "+str(rec)+" "+str(active)+" \n")
+        region = list(ar['areas'])
+        for rg in region:
+            rtotald = rg['totalConfirmed']
+            rdeath = rg['totalDeaths']
+            rrec = rg['totalRecovered']
+            ractive = int(totald)-int(death)-int(rec)
+            rname = rg['displayName']
+            rlat = rg['lat']
+            rlng = rg['long']
+            print(rname+" "+str(rlat)+" "+str(rlng)+" "+str(rtotald)+" "+str(rdeath)+" "+str(rrec)+" "+str(ractive)+" \n")
     
